3531b0978cab308efc19a84a07d14cbf7f94f4fa
Oleg Azemsha <bguproga@gmail.com>
2023-02-19 13:59:43 +0300

    .

--- WebApplication5/Program.cs
+++ WebApplication5/Program.cs
@@ -1,28 +1,26 @@
-//var builder = WebApplication.CreateBuilder(args);
-//var app = builder.Build();
-
-//app.MapGet("/", () => "Hello World!");
-
-
-//int x = 2;
-//app.Run(async (context) =>
-//{
-//    x = x * 2;  //  2 * 2 = 4
-//    await context.Response.WriteAsync($"Result: {x}");
-//});
-//app.Run();
 using WebApplication5;
 
-var service = new ConsoleBackgroundService();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<ConsoleBackgroundService>();
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
 
 var cancellationTokenSource = new CancellationTokenSource();
 var token = cancellationTokenSource.Token;
 
-await service.StartAsync(token);
-
-
-while (Console.ReadKey().Key != ConsoleKey.X)
+app.Run(async (context) =>
 {
+    var service =app.Services.GetRequiredService<ConsoleBackgroundService>();
+    await service.StartAsync(token);
+    while (Console.ReadKey().Key != ConsoleKey.X)
+    {
 
-}
-cancellationTokenSource.Cancel();
+    }
+    cancellationTokenSource.Cancel();
+    await context.Response.WriteAsync($"Stop");
+});
+app.Run();
+
+
+
