ddd37b4699fb6449f47d27f959b7a3c61efc7bc0
Oleg Azemsha <bguproga@gmail.com>
2023-02-20 13:27:21 +0300

    fix

--- WebApplication5/ConsoleBackgroundService.cs
+++ WebApplication5/ConsoleBackgroundService.cs
@@ -4,25 +4,10 @@
     {
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var cancellationTokenSource = new CancellationTokenSource();
-            Task task = Task.Run(() => ReadKeyLoop(cancellationTokenSource));
-            while (!stoppingToken.IsCancellationRequested && !cancellationTokenSource.Token.IsCancellationRequested)
+            while (!stoppingToken.IsCancellationRequested)
             {
                 await Task.Delay(250, stoppingToken);
-                Console.WriteLine(".");
             }
         }
-
-        static void ReadKeyLoop(CancellationTokenSource token)
-        {
-            Console.WriteLine("Вход в цикл чтения клавиатуры.");
-            while (!token.IsCancellationRequested)
-            {
-                if (Console.KeyAvailable)
-                    token.Cancel();
-                Task.Delay(500, token.Token);
-            }
-            Console.WriteLine("Выход из цикла чтения клавиатуры.");
-}
     }
 }
--- WebApplication5/Program.cs
+++ WebApplication5/Program.cs
@@ -9,17 +9,6 @@ app.MapGet("/", () => "Hello World!");
 var cancellationTokenSource = new CancellationTokenSource();
 var token = cancellationTokenSource.Token;
 
-//app.Run(async (context) =>
-//{
-//    var service = app.Services.GetRequiredService<ConsoleBackgroundService>();
-//    await service.StartAsync(token);
-//    while (Console.ReadKey().Key != ConsoleKey.X)
-//    {
-
-//    }
-//    cancellationTokenSource.Cancel();
-//    await context.Response.WriteAsync($"Stop");
-//});
 await app.RunAsync();
 
 
