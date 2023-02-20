f3d5a8dc142f4ce838166adc068d2df4b6a356a4
Oleg Azemsha <bguproga@gmail.com>
2023-02-20 12:01:28 +0300

    add stop

--- WebApplication5/ConsoleBackgroundService.cs
+++ WebApplication5/ConsoleBackgroundService.cs
@@ -4,10 +4,25 @@
     {
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            var cancellationTokenSource = new CancellationTokenSource();
+            Task task = Task.Run(() => ReadKeyLoop(cancellationTokenSource));
+            while (!stoppingToken.IsCancellationRequested && !cancellationTokenSource.Token.IsCancellationRequested)
             {
-                await Task.Delay(500, stoppingToken);
+                await Task.Delay(250, stoppingToken);
+                Console.WriteLine(".");
             }
         }
+
+        static void ReadKeyLoop(CancellationTokenSource token)
+        {
+            Console.WriteLine("Вход в цикл чтения клавиатуры.");
+            while (!token.IsCancellationRequested)
+            {
+                if (Console.KeyAvailable)
+                    token.Cancel();
+                Task.Delay(500, token.Token);
+            }
+            Console.WriteLine("Выход из цикла чтения клавиатуры.");
+}
     }
 }
